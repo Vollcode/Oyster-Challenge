18117a6a5f1705e6b49a656c1034e9dd8750a589
Ruta <ruta@baltiejus.com>
2016-04-19 10:20:29 +0100

    End of challenge 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up money
@@ -15,5 +16,17 @@ class Oystercard
 
   def deduct money
     @balance -= money
+  end
+
+  def in_journey?
+    @in_use
+  end
+
+  def touch_in
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
   end
 end
