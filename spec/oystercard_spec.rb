123b3704949518d0ad18ac1ecb40507267b08027
Daniel Ortiz <danielortiz@Daniels-MacBook-Pro-3.local>
2016-04-19 13:24:47 +0100

    refactored some code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,8 +6,8 @@ describe Oystercard do
    it { expect(subject).to respond_to(:top_up, :touch_in, :touch_out).with(1).argument}
 
    let(:weeksfare) {50}
-   let(:station) { double(:station)}
-   let(:station2) { double(:station2)}
+   let(:entry_station) { double(:entry_station)}
+   let(:exit_station) { double(:exit_station)}
   #  let(:oyster) { double(:oyster, top_up: 50)}
 
    describe "#balance" do
@@ -39,42 +39,42 @@ describe Oystercard do
   describe "#touch_in" do
     it "should touch in" do
       subject.top_up(weeksfare)
-      subject.touch_in station
+      subject.touch_in entry_station
       expect(subject.in_journey?).to eq true
     end
 
     it 'should not allow to touch in if you have insufficient funds' do
-      expect{subject.touch_in(station)}.to raise_error("You have insufficient funds")
+      expect{subject.touch_in(entry_station)}.to raise_error("You have insufficient funds")
     end
   end
 
   describe "#touch_out" do
     it "should touch out" do
-      subject.touch_out(station)
+      subject.touch_out(entry_station)
       expect(subject.in_journey?).to eq false
     end
 
     it "should charge a fee on touch out" do
       subject.top_up(weeksfare)
-      subject.touch_in (station)
-      expect{subject.touch_out(station)}.to change{subject.balance}.by(-Oystercard::STANDARD_FARE)
+      subject.touch_in (entry_station)
+      expect{subject.touch_out(entry_station)}.to change{subject.balance}.by(-Oystercard::STANDARD_FARE)
     end
   end
 
   describe "#travelled_from?" do
-    it "should return station you touched in" do
+    it "should return entry_station you touched in" do
       subject.top_up(weeksfare)
-      subject.touch_in station
-      expect(subject.travelled_from).to eq station
+      subject.touch_in entry_station
+      expect(subject.travelled_from).to eq entry_station
     end
   end
 
   describe '#history' do
-    it 'should show the entry and the exit stations' do
+    it 'should show the entry and the exit entry_stations' do
       subject.top_up(weeksfare)
-      subject.touch_in(station)
-      subject.touch_out(station2)
-      expect(subject.history).to eq [station, station2]
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.history).to eq [entry_station, exit_station]
     end
   end
 end
